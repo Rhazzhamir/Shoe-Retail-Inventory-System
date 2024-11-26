f8742fcc9b255c70b1b00c2e4189363113ee6ccb
RHAZZHAMIR AGUILAR <aguilarrhazzhamir@gmail.com>
2024-11-26 10:55:03 +0800

    'alert'

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate , logout
 from .form import CustomUserCreationForm , CustomAuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm
+from django.contrib import messages
 # Create your views here.
 
 def register_view(request):
@@ -12,7 +13,8 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('Customer:user_dashboard')
+            messages.success(request , 'Registration successful! Please log in.')
+            return redirect('accounts:login')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html', {'form': form})
@@ -23,6 +25,7 @@ def login_view(request):
         if form.is_valid():
             user = form.get_user()
             login(request , user)
+            messages.success(request , 'Login Successful')
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
             else:
@@ -35,6 +38,8 @@ def login_view(request):
 def logout_view(request):
     if request.method == 'POST':
         logout(request)
+        messages.success(request, 'Logout successful! You have been logged out.')
+        return redirect('accounts:login')
     return redirect('accounts:login')
 
 # Profile
@@ -44,7 +49,7 @@ def profile_view(request):
     return render(request , 'profile.html')
 
 
-@login_required
+@login_required(login_url='accounts:login')
 def user_profile(request):
     if request.method == 'POST':
         form = UserChangeForm(request.POST, instance=request.user)
--- user/static/js/scroller.js
+++ user/static/js/scroller.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
 const scrollers = document.querySelectorAll(".scroller");
 
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
