4fbe51cc9ed3326c04f587030fcfac312fc8b546
RHAZZHAMIR AGUILAR <aguilarrhazzhamir@gmail.com>
2025-01-09 10:40:16 +0800

    'add_product_picture'

--- seller/admin.py
+++ seller/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Category , Product
-# Register your models here.
+from .models import Category, Product, DeletedCategory, DeletedProduct
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'created_at', 'updated_at')
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'created_at', 'updated_at')
+
+class DeletedCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'deleted_at')
+
+class DeletedProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'deleted_at')
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(DeletedCategory, DeletedCategoryAdmin)
+admin.site.register(DeletedProduct, DeletedProductAdmin)
