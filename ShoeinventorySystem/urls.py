241e79e55ffaa3bec19fefdc7246ced753fc9087
RHAZZHAMIR AGUILAR <aguilarrhazzhamir@gmail.com>
2025-01-18 13:49:04 +0800

    'pinaka_last_push''

--- ShoeinventorySystem/urls.py
+++ ShoeinventorySystem/urls.py
@@ -9,7 +9,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls' )),
+    # path('accounts/', include('accounts.urls' )),
     path('accounts/' , include('accounts.urls')),
     path('user/', include('user.urls')),
     path('seller/' , include('seller.urls')),
